0a6bb3d9ec93d9aad7ef937c0c38af77bd7c1e8a
PKurianski <pedrokurianski@gmail.com>
2023-05-15 19:02:21 -0300

    DataAccessObject-pattern

---
+++ src/DAO/AccessBook.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import DAO.service.impl.BookDaoImpl;
+import DAO.service.BookDao;
+import DAO.service.Books;
+
+public class AccessBook {
+
+    public static void main(String[] args) {
+
+        BookDao bookDao = new BookDaoImpl();
+
+        for (Books book : bookDao.getAllBooks()) {
+            System.out.println("Book ISBN : " + book.getIsbn());
+        }
+
+        //update student
+        Books book = bookDao.getAllBooks().get(1);
+        book.setBookName("Algorithms");
+        bookDao.saveBook(book);
+    }
+}
